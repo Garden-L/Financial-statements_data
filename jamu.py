a7c7e67332937bee85d8e0de6e5510ceea1fe984
JeongWon Lim <cdnljw@gmail.com>
2020-12-06 22:19:55 +0900

    계정과목 분류

--- jamu.py
+++ jamu.py
@@ -19,36 +19,33 @@ class jamu:
         #원본 복제
         self.dfCopyList = [ i.copy() for i in self.df_list]
         
-        modify =pd.DataFrame()
-
-        for df in self.dfCopyList:
-            modify = pd.concat([modify,df[['항목코드','항목명']]], axis=0)
-        
-        modify = modify[~modify['항목코드'].str.contains(r'^(entity)')]
-        modify.drop_duplicates('항목명', inplace=True)
-        modify.set_index('항목명')
-
+        modify =pd.read_excel('계정과목3.xlsx')
+        modify.set_index('항목명', inplace=True)
+        ifrsmodify =pd.read_excel('ifrs계정과목.xlsx')
+        ifrsmodify.set_index('항목코드', inplace=True)
         i = 0
         for df in self.dfCopyList:
             #컬럼 삭제 ['재무제표종류', '당기 1분기 3개월  '당기 1분기 누적', '전기 1분기 3개월', '전기 1분기 누적', '전기', '전전기']
             df.drop(columns=list(df.columns[0:1]) +list(df.columns[13:]), inplace=True) 
             
             #항목명 띄어쓰기 삭제
-            # df['항목명'] = df['항목명'].str.replace(' ', '')
+            df['항목명'] = df['항목명'].str.replace(' ', '')
 
             #형변환 (문자형)
             df=df.astype(str)
             #entity항목 수정 -> entity[기업코드]
-            entity=df.loc[df['항목코드'].str.contains('^entity'),'항목코드'].str.slice(0,14)
-            df.loc[entity.index,['항목코드']] = entity
 
+            # entity=df.loc[df['항목코드'].str.contains(r'^e'),'항목코드'].str.slice(0,14)
+            df.loc[df['항목코드'].str.contains(r'^e'),'항목코드'] = float('NaN')
             #entity
-            tempIndex = df[df['항목코드'].str.contains(r'^(entity)')]
+            # tempIndex = df.loc[df['항목코드'].str.contains(r'^e'),:]
 
-            tempIndex.set_index('항목명', inplace=True)
-            tempIndex.update(modify['항목코드'])
-            tempIndex.reset_index(inplace=True)
-            
+            df.set_index('항목명', inplace=True)
+            df.update(modify, overwrite=False)
+            df.reset_index(inplace=True)
+            df.set_index('항목코드', inplace=True)
+            df.update(ifrsmodify)
+            df.reset_index(inplace=True)
             df = df[['종목코드', '회사명', '시장구분', '업종', '업종명', '결산월', '결산기준일', '보고서종류', '통화', '항목코드', '항목명', '당기 1분기 3개월']]
             #재무재표 통합 : 상위파일에 없는 계정은 하위파일 계정으로 합산
             if len(self.mergeJamu) == 0:
@@ -61,7 +58,7 @@ class jamu:
                 self.mergeJamu.drop(columns='당기 1분기 3개월_0', inplace=True)
                 self.mergeJamu.rename(columns={'당기 1분기 3개월_1' : '당기 1분기 3개월'}, inplace=True)
 
-            
+            i+=1
                 # print(len(set(self.mergeJamu['종목코드'])))
         # print(self.mergeJamu.columns[-4:])
         # for col in self.mergeJamu.columns[-5:]:
@@ -74,6 +71,7 @@ class jamu:
         #     self.mergeJamu = self.mergeJamu.combine_first(df)
         
         self.mergeJamu.to_csv('merge.csv', encoding='cp949')
+
 
 
 if __name__ == '__main__':
